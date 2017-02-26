b01a6bb26778d0b1aa507ed47493e2c4b9482505
Steven Penny <svnpenn@gmail.com>
2017-02-25 23:26:29 -0600

    case $1 in

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/dash -e
-ln -sfv "$PWD"/stdlib /usr/local/bin
+ln -sfv "$PWD"/awklib "$PWD"/shlib /usr/local/bin
 mkdir -pv /usr/local/share
-ln -sfv "$PWD"/libstd.awk "$PWD"/libstd.sh /usr/local/share
+ln -sfv "$PWD"/libstd.awk "$PWD"/libstd.sh "$PWD"/stdlib.txt /usr/local/share
