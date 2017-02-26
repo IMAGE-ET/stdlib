cebbada68d5aae5b60a362e1f634769af21d6cdc
Steven Penny <svnpenn@gmail.com>
2017-02-26 14:37:03 -0600

    say() {

---
+++ t/stdlib.sh
@@ -0,0 +1,77 @@
+#!/bin/dash -e
+say() {
+  echo
+  echo "test $1"
+}
+x=$(mktemp)
+chmod +x "$x"
+
+say 1
+shlib -v
+
+say 2
+shlib -c 'ceil 2.1'
+
+say 3
+shlib -c 'ceil "$1"' 2.1
+
+say 4
+echo 'ceil 2.1' > "$x"
+shlib -f "$x"
+
+say 5
+echo 'ceil "$1"' > "$x"
+shlib -f "$x" 2.1
+
+say 6
+cat > "$x" <<'eof'
+#!/usr/local/bin/shlib -f
+ceil 2.1
+eof
+"$x"
+
+say 7
+cat > "$x" <<'eof'
+#!/usr/local/bin/shlib -f
+ceil "$1"
+eof
+"$x" 2.1
+
+say 8
+awklib -v
+
+say 9
+awklib -c 'BEGIN {print ceil(2.1)}'
+
+say 10
+awklib -c 'BEGIN {print ceil(ARGV[1])}' 2.1
+
+say 11
+cat > "$x" <<'eof'
+2.1
+3.1
+4.1
+eof
+awklib -c '{print ceil($0)}' "$x"
+
+say 12
+echo 'BEGIN {print ceil(2.1)}' > "$x"
+awklib -f "$x"
+
+say 13
+echo 'BEGIN {print ceil(ARGV[1])}' > "$x"
+awklib -f "$x" 2.1
+
+say 14
+cat > "$x" <<'eof'
+#!/usr/local/bin/awklib -f
+BEGIN {print ceil(2.1)}
+eof
+"$x"
+
+say 15
+cat > "$x" <<'eof'
+#!/usr/local/bin/awklib -f
+BEGIN {print ceil(ARGV[1])}
+eof
+"$x" 2.1
